dd9b295aa4bfb9a20ba0a71fdebbaaab0af41364
Daniel Rees <daniel_james_rees@hotmail.com>
2021-03-28 20:01:53 -0400

    Calls openweathermap api for a specific city and caches the response for 30 minutes

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/danrees/pi-backend/pkg/weather"
+	"github.com/joho/godotenv"
+)
+
+type Cache struct {
+	mu     *sync.Mutex
+	item   *weather.Weather
+	stamp  *time.Time
+	client *weather.Client
+}
+
+func (c *Cache) get() (*weather.Weather, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if (c.item == nil || c.stamp == nil) || time.Now().After(c.stamp.Add(30*time.Minute)) {
+		stamp := time.Now()
+		c.stamp = &stamp
+		w, err := c.client.Get()
+		if err != nil {
+			return nil, err
+		}
+		c.item = w
+	}
+	return c.item, nil
+}
+
+type Server struct {
+	cache *Cache
+}
+
+func NewServer(cityID, apiKey string) *Server {
+	server := Server{
+		cache: &Cache{
+			mu:     &sync.Mutex{},
+			client: weather.New("https://api.openweathermap.org", cityID, apiKey),
+		},
+	}
+	return &server
+}
+
+func (s *Server) getWeather(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "unsupported method", http.StatusBadRequest)
+		return
+	}
+	wr, err := s.cache.get()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := json.NewEncoder(w).Encode(wr); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No env file found: ", err.Error())
+	}
+	apiKey, ok := os.LookupEnv("WEATHER_API_KEY")
+	if !ok {
+		log.Fatal("You must provide an open weather map api key")
+	}
+	cityID, ok := os.LookupEnv("WEATHER_CITY_ID")
+	if !ok {
+		log.Fatal("You must provide an open weather map city id")
+	}
+	server := NewServer(cityID, apiKey)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/weather", server.getWeather)
+	log.Println("Starting server on port 8000")
+	if err := http.ListenAndServe(":8000", mux); err != nil {
+		panic(err)
+	}
+}
---
+++ pkg/weather/client.go
@@ -0,0 +1,97 @@
+package weather
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+func New(addr, cityID, apiKey string) *Client {
+	return &Client{
+		addr:   addr,
+		apiKey: apiKey,
+		cityID: cityID,
+		client: &http.Client{
+			Timeout: 30 * time.Second,
+		},
+	}
+}
+
+type Client struct {
+	client *http.Client
+	addr   string
+	apiKey string
+	cityID string
+}
+
+func (c *Client) Get() (*Weather, error) {
+	addr := fmt.Sprintf("%s/data/2.5/weather", c.addr)
+
+	link, err := url.Parse(addr)
+	if err != nil {
+		return nil, err
+	}
+	params := link.Query()
+	params.Add("id", c.cityID)
+	params.Add("appid", c.apiKey)
+	link.RawQuery = params.Encode()
+
+	resp, err := http.Get(link.String())
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode < 200 || resp.StatusCode > 399 {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+	w := new(Weather)
+	if err := json.NewDecoder(resp.Body).Decode(w); err != nil {
+		return nil, err
+	}
+
+	return w, nil
+}
+
+type Weather struct {
+	Coord struct {
+		Lon float64 `json:"lon,omitempty"`
+		Lat float64 `json:"lat,omitempty"`
+	} `json:"coord,omitempty"`
+	Weather []struct {
+		Main        string `json:"main,omitempty"`
+		Description string `json:"description,omitempty"`
+		Icon        string `json:"icon,omitempty"`
+		ID          int64  `json:"id,omitempty"`
+	} `json:"weather,omitempty"`
+	Base string `json:"base,omitempty"`
+	Main struct {
+		Temp      float64 `json:"temp,omitempty"`
+		FeelsLike float64 `json:"feels_like,omitempty"`
+		TempMin   float64 `json:"temp_min,omitempty"`
+		TempMax   float64 `json:"temp_max,omitempty"`
+		Pressure  int64   `json:"pressure,omitempty"`
+		Humidity  int64   `json:"humidity,omitempty"`
+	} `json:"main,omitempty"`
+	Visibility int64 `json:"visibility,omitempty"`
+	Wind       struct {
+		Speed float64 `json:"speed,omitempty"`
+		Deg   int64   `json:"deg,omitempty"`
+		Gust  float64 `json:"gust,omitempty"`
+	} `json:"wind,omitempty"`
+	Clouds struct {
+		All int `json:"all,omitempty"`
+	} `json:"clouds,omitempty"`
+	DT  int64 `json:"dt,omitempty"`
+	Sys struct {
+		Type    int64  `json:"type,omitempty"`
+		ID      int64  `json:"id,omitempty"`
+		Country string `json:"country,omitempty"`
+		Sunrise int64  `json:"sunrise,omitempty"`
+		Sunset  int64  `json:"sunset,omitempty"`
+	} `json:"sys,omitempty"`
+	Timezone int64  `json:"timezone,omitempty"`
+	ID       int64  `json:"id,omitempty"`
+	Name     string `json:"name,omitempty"`
+	COD      int64  `json:"cod,omitempty"`
+}
